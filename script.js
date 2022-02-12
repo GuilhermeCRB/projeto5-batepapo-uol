8be8a6d232743f7ea220ab5ef08cb8ac5a88a894
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-02-12 11:58:44 -0300

    Projeto Driven Bate-papo UOL versão com implementação do requisito Chat

--- script.js
+++ script.js
@@ -1,67 +1,63 @@
 let user = { name: prompt("Qual nome de usuário gostaria de utilizar?") };
 const chat = document.querySelector(".chat");
 let date; let time = { hours: "", minutes: "", seconds: "" };
-let intervalStatus;
+let intervalStatus; let intervalMessages;
 
 checkUserName();
 
 function checkUserName() {
     const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", user);
-    promise.then(loadChat); promise.catch(warnError);
+    promise.then(checkFunctions); promise.catch(warnError);
+}
+
+function checkFunctions() {
+    intervalStatus = setInterval(checkUserStatus, 5000);
+    intervalMessages = setInterval(checkForNewMessages, 3000);
 }
 
 function checkUserStatus() {
     if (window.closed) {
         clearInterval(intervalStatus);
-        loadChat("User exit");
     } else {
         const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", user);
     }
 }
-
 
 function warnError(error) {
     user.name = prompt(`Erro ${error.response.status}. Por favor, insira um nome válido.\nQual nome de usuário gostaria de utilizar?`);
     checkUserName();
 }
 
-function loadChat(response) {
-    updateTime(); console.log(response);
-
-    if (response === "User exit") {
-        chat.innerHTML +=
-            `
-                <p class="message chat_status-message">
-                    <span class="time">(${time.hours}:${time.minutes}:${time.seconds})</span> <span class="user-name">${user.name}</span> saiu da sala...
-                </p>
-            `;
-    } else {
-        chat.innerHTML +=
-            `
-                <p class="message chat_status-message">
-                    <span class="time">(${time.hours}:${time.minutes}:${time.seconds})</span> <span class="user-name">${user.name}</span> entra na sala...
-                </p>
-            `;
-
-        intervalStatus = setInterval(checkUserStatus, 5000);
-    }
+function checkForNewMessages() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    promise.then(loadChat);
 }
 
-function updateTime() {
-    date = new Date();
-    time.hours = date.getHours(); time.minutes = date.getMinutes(); time.seconds = date.getSeconds();
-
-    if (time.hours < 10) {
-        time.hours = "0" + time.hours.toString();
+function loadChat(response) {
+    for (let i = 0; i < response.data.length; i++) {
+        if (response.data[i].type === "status") {
+            chat.innerHTML +=
+                `
+            <p class="message chat_${response.data[i].type}">
+                <span class="time">(${response.data[i].time})</span> <span class="user-name">${response.data[i].from}</span> ${response.data[i].text}
+            </p>
+            `;
+        } else if (response.data[i].type !== "private_message" || response.data[i].to === user.name || response.data[i].from === user.name) {
+            chat.innerHTML +=
+                `
+            <p class="message chat_${response.data[i].type}">
+                <span class="time">(${response.data[i].time})</span> <span class="user-name">${response.data[i].from}</span> para <span class="user-name">${response.data[i].to}</span>: &nbsp;${response.data[i].text}
+            </p>
+            `;
+        }
     }
+    scrolltoLastMessage();
+}
 
-    if (time.minutes < 10) {
-        time.minutes = "0" + time.minutes.toString();
-    }
-
-    if (time.seconds < 10) {
-        time.seconds = "0" + time.seconds.toString();
-    }
+function scrolltoLastMessage(){
+    let messageArray = [...chat.querySelectorAll(".message")];
+    messageArray[messageArray.length -1].scrollIntoView();
+    console.log(messageArray[messageArray.length -1])
 }
 
 function openMessageMenu(element) {
