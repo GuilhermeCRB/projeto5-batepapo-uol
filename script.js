9c50028c7ef26d15a047853adaf2b3fbf8aba853
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-02-12 12:57:06 -0300

    Projeto Driven Bate-papo UOL versão com requisito Envio de mensagem e Critérios para correção automática

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let user = { name: prompt("Qual nome de usuário gostaria de utilizar?") };
 const chat = document.querySelector(".chat");
-let date; let time = { hours: "", minutes: "", seconds: "" };
 let intervalStatus; let intervalMessages;
 
 checkUserName();
@@ -23,14 +22,19 @@ function checkUserStatus() {
     }
 }
 
+function checkForNewMessages() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    promise.then(loadChat);
+}
+
 function warnError(error) {
     user.name = prompt(`Erro ${error.response.status}. Por favor, insira um nome válido.\nQual nome de usuário gostaria de utilizar?`);
     checkUserName();
 }
 
-function checkForNewMessages() {
-    const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-    promise.then(loadChat);
+function reloadWindow(error) {
+    alert(`Erro ${error.response.status}. Erro de conexão, a página precisa ser recarregada.`)
+    window.location.reload();
 }
 
 function loadChat(response) {
@@ -38,14 +42,14 @@ function loadChat(response) {
         if (response.data[i].type === "status") {
             chat.innerHTML +=
                 `
-            <p class="message chat_${response.data[i].type}">
+            <p class="message chat_${response.data[i].type}" data-identifier="message">
                 <span class="time">(${response.data[i].time})</span> <span class="user-name">${response.data[i].from}</span> ${response.data[i].text}
             </p>
             `;
         } else if (response.data[i].type !== "private_message" || response.data[i].to === user.name || response.data[i].from === user.name) {
             chat.innerHTML +=
                 `
-            <p class="message chat_${response.data[i].type}">
+            <p class="message chat_${response.data[i].type}" data-identifier="message">
                 <span class="time">(${response.data[i].time})</span> <span class="user-name">${response.data[i].from}</span> para <span class="user-name">${response.data[i].to}</span>: &nbsp;${response.data[i].text}
             </p>
             `;
@@ -54,10 +58,21 @@ function loadChat(response) {
     scrolltoLastMessage();
 }
 
-function scrolltoLastMessage(){
+function scrolltoLastMessage() {
     let messageArray = [...chat.querySelectorAll(".message")];
-    messageArray[messageArray.length -1].scrollIntoView();
-    console.log(messageArray[messageArray.length -1])
+    messageArray[messageArray.length - 1].scrollIntoView();
+}
+
+function sendMessage() {
+    let message = {
+        from: user.name,
+        to: "Todos",
+        text: document.querySelector("input").value,
+        type: "message"
+    }
+    const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", message)
+    promise.then(checkForNewMessages); promise.catch(reloadWindow);
+    document.querySelector("input").value = "";
 }
 
 function openMessageMenu(element) {
